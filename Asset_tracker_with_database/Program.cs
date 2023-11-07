f71fa26e0050d8abb98996b1e1dd3422ef168951
Tallysing <tallysing@tutanota.com>
2023-11-07 15:40:28 +0100

    Before Creating a Database

--- Asset_tracker_with_database/Program.cs
+++ Asset_tracker_with_database/Program.cs
@@ -18,6 +18,41 @@ while (true)
     Console.Write("Please enter the asset model: ");
 
     asset.AssetModel = Console.ReadLine();
-    
 
+    Console.Write("Please provide the location of the office; Sweden, Spain or USA: ");
+
+    asset.Office = Console.ReadLine();
+
+    Console.Write("Use this format (dd/mm/yyyy) to type the purchase date of the asset: ");
+
+    DateTime date;
+
+    while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out date)) // Verify valid date format
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        Console.Write("You have entered an invalid date format. Please try again (dd/mm/yyyy): ");
+
+        Console.ResetColor();
+    }
+    asset.PurchaseDate= date;
+
+    Console.Write("Please fill in the dollar price of the asset: ");
+
+    int price;
+
+    while (!int.TryParse(Console.ReadLine(), out price)) // Valid number verification
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        Console.WriteLine("Only whole numbers are permitted, so please try again: ");
+
+        Console.ResetColor();
+
+    }
+    asset.USDprice = price;
+
+    Context.Asssets.Add(asset);
+
+    Context.SaveChanges();
 }
