821dcc516dc0e76acdd173d7c362c28910bd6e02
Tallysing <tallysing@tutanota.com>
2023-11-07 15:07:01 +0100

    Add project files.

---
+++ Asset_tracker_with_database/Asset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_tracker_with_database
+{
+    internal class Asset
+    {
+        public Asset(string type, string brand, string assetModel, string office, DateTime purchaseDate, int usdPrice)
+        {
+            Type = type;
+            Brand = brand;
+            AssetModel = assetModel;
+            Office = office;
+            PurchaseDate = purchaseDate;
+            USDprice = usdPrice;
+        }
+        public string Type { get; }
+        public string Brand { get; }
+        public string AssetModel { get; }
+        public string Office { get; }
+        public DateTime PurchaseDate { get; }
+        public int USDprice { get; }
+    }
+}
---
+++ Asset_tracker_with_database/AssetDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asset_tracker_with_database
+{
+    internal class AssetDbContext:DbContext
+    {
+    }
+}
