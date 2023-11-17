29e176d999d1e09add75e9b05cec80d9f76ae213
Tallysing <tallysing@tutanota.com>
2023-11-17 14:32:41 +0100

    Created office check

---
+++ Asset_tracker_with_database/InputCheckers.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Asset_tracker_with_database
+{
+    internal class InputCheckers
+    {
+        bool OfficeCheck(string input)
+        {
+
+            switch (input)
+            {
+                case "[Ss]pain": return true;
+
+                case "[Ss]weden": return true;
+
+                case @"\b(U\.?S\.?A\.?)\b": return true;
+
+                    default : return false;
+            }
+        }
+    }
+}
